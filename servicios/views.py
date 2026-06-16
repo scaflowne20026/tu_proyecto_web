eace777e8e70f25073a318fc0772d442295ad488
Juan Carlos <cajadenas@gmail.com>
2026-06-15 22:14:13 -0500

    código completo vistas

--- servicios/views.py
+++ servicios/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Servicio
 
 def inicio(request):
-    return render(request, 'inicio.html')
+    servicios = Servicio.objects.all()
+    return render(request, 'inicio.html', {'servicios': servicios})
 
 def detalle_servicio(request, pk):
     servicio = get_object_or_404(Servicio, pk=pk)

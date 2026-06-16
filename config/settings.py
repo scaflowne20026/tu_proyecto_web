0d05e0dede7d097c706453b4520ae35d5a0401df
Juan Carlos <cajadenas@gmail.com>
2026-06-16 15:09:08 -0500

    Habilita visualización de imágenes

--- config/settings.py
+++ config/settings.py
@@ -85,3 +85,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'cajadenas@gmail.com'
 EMAIL_HOST_PASSWORD = 'tu_contraseña_de_aplicacion'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
